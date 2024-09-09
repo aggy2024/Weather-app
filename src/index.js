d506cb373f8cdbde942a2e4c4f7f7e7a2000d0c5
aggy2024 <agnessmakoni@gmail.com>
2024-09-09 17:07:11 +0200

    added functions

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#city");
   cityElement = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
- 
 }
 
 function searchCity(city) {
-  let apiKey = '05fb6tf1c13f54cffb7783239f7f69oa';
-  let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey&units=metric';
+  let apiKey = "05fb6tf1c13f54cffb7783239f7f69oa";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey&units=metric";
   axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  
+
   cityElement.innerHTML = searchInput.value;
-  searchCity(searchInput.value)
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
