a75c31430220fdccb8b329b6421aae240f2a4a3a
aggy2024 <agnessmakoni@gmail.com>
2024-09-10 11:46:45 +0200

    corrections

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ function formatDays(date) {
 function searchCity(city) {
   let apiKey = "05fb6tf1c13f54cffb7783239f7f69oa";
   let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}";
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
   axios.get(apiUrl).then(refreshWeather);
 }
 
