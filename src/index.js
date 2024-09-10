0d4ca720f2505a3e00093a69314e559b1f42f453
aggy2024 <agnessmakoni@gmail.com>
2024-09-10 09:06:26 +0200

    removed errors from js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = response.data.temperature.current;
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  cityElement = response.data.city;
+
+
+  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
   let apiKey = "05fb6tf1c13f54cffb7783239f7f69oa";
   let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey&units=metric";
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -17,7 +19,6 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
 
-  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("search-form");
