5a9bce21541ef9e3c1c186f3d89e1c0a636eb4f0
aggy2024 <agnessmakoni@gmail.com>
2024-09-12 12:10:20 +0200

    created a new function to the index.js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = '${response.data.wind.speed}km/h';
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = '<img src="${response.data.condition.icon_url}" class="weather-app-icon" />'
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -50,9 +52,15 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-function displayForecast() {
-  
+function getForecast(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433"; 
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 
+
+function displayForecast(response) {
+  console.log(response.data);
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = '';
   days.forEach(function (day) {
@@ -60,7 +68,7 @@ function displayForecast() {
       forecastHtml +
       
       < div class="weather-forecast-day" >
-        <div class="weather-forecast-date">${day}</div>
+     <div class="weather-forecast-date">${day}</div>
     <div class="weather-forecast-icon">⛅</div>
     <div class="weather-forecast-temperatures">
       <div class="weather-forecast-temperature">
@@ -68,7 +76,7 @@ function displayForecast() {
       </div>
       <div class="weather-forecast-temperature">9°</div>
     </div>
-    </div >
+    </div>
     ;
       
   });
@@ -79,6 +87,6 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
-displayForecast();
+
 
 
