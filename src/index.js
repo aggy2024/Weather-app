f021274441830ce0edeec59dddcad081a11ba165
aggy2024 <agnessmakoni@gmail.com>
2024-09-12 15:02:32 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -67,28 +67,29 @@ function getForecast(city) {
 
 
 function displayForecast(response) {
-  let forecastHtml = "";
+ forecastHtml = "";
 
-
-  response.data.daily.forEach(function (day, index) {
-    if (index < 5) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function(day,index) {
+   if (index < 5) {
+     forecastHtml = 
+     forecastHtml +
+       `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${formatDate(day.time)}</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
 
           <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
           <div class="weather-forecast-temperatures">
             <div class="weather-forecast-temperature">
               <strong>${Math.round(day.temperature.maximum)}°</strong>
             </div>
-            <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
+            <div class="weather-forecast-temperature">${Math.round(
+              day.temperature.minimum
+            )}°</div>
           </div>
         </div>
     `;
-  }  
-  });
+   }
+ });
 
 
   let forecastElement= document.querySelector("#forecast");
